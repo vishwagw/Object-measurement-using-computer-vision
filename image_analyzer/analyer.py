e8e413861a24c8c0fc6be5edebfa35477b9d4ece
Vishwa Randy GW <136871744+vishwagw@users.noreply.github.com>
2025-08-25 08:31:56 +0530

    Update analyer.py

--- image_analyzer/analyer.py
+++ image_analyzer/analyer.py
@@ -21,3 +21,28 @@ image = cv2.imread(img_path)
 
 # resizing:
 
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blur = cv2.GaussianBlur(gray, (9, 9), 0)
+
+edged = cv2.Canny(blur, 50, 100)
+edged = cv2.dilate(edged, None, iterations=1)
+edged = cv2.erode(edged, None, iterations=1)
+
+#show_images([blur, edged])
+
+# Find contours
+cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+
+# Sort contours from left to right as leftmost contour is reference object
+(cnts, _) = contours.sort_contours(cnts)
+
+# Remove contours which are not large enough
+cnts = [x for x in cnts if cv2.contourArea(x) > 100]
+
+#cv2.drawContours(image, cnts, -1, (0,255,0), 3)
+
+#show_images([image, edged])
+#print(len(cnts))
+
+
