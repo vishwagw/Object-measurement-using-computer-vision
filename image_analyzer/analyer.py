5fa9176d72d9a941b8bd3b57df308c982b8f7d70
Vishwa Randy GW <136871744+vishwagw@users.noreply.github.com>
2025-08-25 08:31:28 +0530

    Update analyer.py

--- image_analyzer/analyer.py
+++ image_analyzer/analyer.py
@@ -6,3 +6,18 @@ from scipy.spatial.distance import euclidean
 from imutils import perspective
 from imutils import contours
 
+# function to show arrat of images:
+def show_images(images):
+	for i, img in enumerate(images):
+		cv2.imshow("image_" + str(i), img)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
+# include the path to the image you want to analyze:
+img_path = "obj2.jpeg"
+
+# Read image and preprocess
+image = cv2.imread(img_path)
+
+# resizing:
+
